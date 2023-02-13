ac022f1af4c4d4ca16b5eacac320e794487f08b8
wenliangsu <wenliangsu65018@gmail.com>
2023-02-14 00:16:01 +0800

    Fix: modified the bug of passport setting

--- config/passport.js
+++ config/passport.js
@@ -47,7 +47,7 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET,
-      callbackURL: 'http://localhost:3000/auth/facebook/callback',
+      callbackURL: process.env.FACEBOOK_CALLBACK,
       profileFields: ['email', 'displayName']
     },
     function (accessToken, refreshToken, profile, cb) {
